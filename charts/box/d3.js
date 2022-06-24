9501c1c681bcd54600cc8c9eb0bee24e6ffc2496
b0000ring <b0000ring@users.noreply.github.com>
2022-06-24 17:36:03 +0000

    deploy: 3fbf37b8115cb3d4dd902e9dab0a96cf7abdcf32

---
+++ charts/box/d3.js
@@ -0,0 +1,141 @@
+function makeBox() {
+  //empty data set
+  let data = new Array(10).fill()
+
+  // max data item value
+  const max = 15
+  // min data item value
+  const min = 5
+  const padding = 10
+
+  // selecting root element for plot
+  const svg = d3.select('#chart')
+  // getting root element width
+  const width = parseInt(svg.style('width')) 
+  // getting root element height
+  const height = parseInt(svg.style('height')) 
+
+  // creating scale for Y axis
+  // because x axis will be static
+  const scaleY = d3.scaleLinear()
+    .domain([0, max + 5])
+    .range([padding, height - padding])
+
+  // creating axis with scale usage
+  const axis = d3.axisRight(scaleY)
+  // creating wrapper for plot items
+  const items = svg.append('g')
+
+  // creating vertical helping line
+  items.append('line')
+    // applying coords
+    .attr('x1', width / 2)
+    .attr('y1', padding)
+    .attr('x2', width / 2)
+    .attr('y2', height-padding)
+    // applying line color
+    .style('stroke', '#a9a9b3')
+    // applying line style (dashing)
+    .style('stroke-dasharray', '4')
+
+  // creating top horizontal helping line
+  items.append('line')
+    // most of the attributes the same
+    // as for vertical line
+    .attr('x1', width / 3)
+    .attr('y1', padding)
+    .attr('x2', width / 1.5)
+    .attr('y2', padding)
+    .attr('stroke', '#a9a9b3')
+  // creating bottom horizontal helping line
+  items.append('line')
+    .attr('x1', width / 3)
+    .attr('y1', height - padding)
+    .attr('x2', width / 1.5)
+    .attr('y2', height - padding)
+    .attr('stroke', '#a9a9b3')
+
+  // creating rect element for box
+  // we dont really need to connect data
+  // to the element itself
+  // so there is no direct data binding steps
+  items.append('rect')
+    // applying id
+    .attr('id', 'box')
+    // setting x coord (its static)
+    .attr('x', width / 4)
+    // setting y coord
+    // using scaling to get correct positioning from
+    // minimum data value (because this is a beginning point) 
+    .attr('y', scaleY(d3.min(data)))
+    // setting width
+    .attr('width', width / 2)
+    // setting rect fill color
+    .style('fill', '#a9a9b3')
+    // setting stroke color
+    .style('stroke', 'black')
+
+  // creating line for mean value
+  items.append('line')
+    // setting id to element
+    .attr('id', 'mean')
+    // setting line x1 coord (its constant)
+    .attr('x1', width / 4)
+    // setting initial y1 coord
+    // (-10 to hide the line for initial render)
+    .attr('y1', -10)
+    // setting x2 coord (its constant)
+    // also defines the line width
+    .attr('x2', width / 2 + width / 4)
+    // setting initial y2 coord
+    // (-10 to hide the line for initial render)
+    .attr('y2', -10)
+    // setting stroke color
+    .style('stroke', 'black')
+    // setting stroke width
+    .style('stroke-width', '4')
+
+  // creating g element for Y axis and calling its component function
+  svg.append('g')
+    .call(axis)
+
+  // function for showing dynamic data and view change
+  // it generates new data, applies to plot and
+  // changes the view with animation
+  function change() {
+    // selecting box element by id
+    const rect = d3.select('#box')
+    // select mean line element by id
+    const line = d3.select('#mean')
+    // updating of dataset
+    data = data.map(() => Math.floor(Math.random() * (max - min)) + min)
+    // calculation of mean value from data
+    const mean = d3.mean(data)
+    
+    // changing elements attributes based on new data
+    rect
+        // creating transition
+        .transition() 
+        // setting transition duration
+        .duration(300)
+        // applying new y coord
+        .attr('y', scaleY(d3.min(data)))
+        // applying height to box element
+        // scaled min value should be substracted from scaled max
+        // to get correct box size
+        .attr('height', scaleY(d3.max(data)) - scaleY(d3.min(data)))
+
+    line.transition() 
+      // setting transition duration
+      .duration(300)
+      // setting new y1 coord
+      .attr('y1', scaleY(mean))
+      // setting new y2 coord
+      .attr('y2', scaleY(mean))
+  }
+
+  // calling change function every second
+  setInterval(change, 1000)
+}
+
+window.onload = makeBox
---
+++ charts/tree/d3.js
@@ -0,0 +1,209 @@
+function makeTree() {
+  //data set
+  const data = {
+    name: 'root',
+    color: '#ABDDA4',
+    children: [
+      {name: 'child 1', color: '#5E4FA2'},
+      {
+        name: 'child 2',
+        color: '#FDAE61',
+        children: [
+          {name: 'child 4', color: '#3288BD'},
+          {name: 'child 5', color: '#66C2A5'}
+        ]
+      },
+      {
+        name: 'child 3',
+        color: '#E6F598',
+        children: [
+          {name: 'child 6', color: '#FEE08B'}
+        ]
+      }
+    ]
+  }
+
+  // selecting root element for plot
+  const svg = d3.select('#chart')
+    // setting cursor style
+    .style('cursor', 'pointer')
+
+  // getting root element width
+  const width = parseInt(svg.style('width')) 
+  // getting root element height
+  const height = parseInt(svg.style('height')) 
+
+  // tree node height
+  const nodeHeight = 50
+  // tree node size
+  const nodeWidth = 120
+  // length of space between nodes
+  const nodeSeparation = 30
+
+  // creating zoom behaviour
+  const zoom = d3.zoom()
+      // setting extent for zooming
+      .scaleExtent([1, 2])
+      // setting callback event (both scrolling and zooming)
+      .on("zoom", onzoom)
+
+  // creating tree hierarchy from the data
+  const nodes = d3.hierarchy(data, d => d.children)
+  // creating generator for link paths
+  // (x and y coords should be swapped for horizontal rendering)
+  const lnkMkr = d3.linkHorizontal().x( d => d.y ).y( d => d.x )
+
+  // creating of common component for all plot elements
+  const g = svg.append('g')
+
+  svg
+    // setting initial transform for plot
+    .call(
+      // calling zoom.transform manually
+      zoom.transform,
+      // passing initial transform params by calling translate func
+      d3.zoomIdentity.translate(width / 4, height / 2)
+    )
+    // applying zooming behaviour to plot
+    .call(zoom)
+
+  // creating defs element for plot
+  // which contain base element 
+  // for tree nodes
+  svg.append('defs')
+    // creating base tree node element
+    .append('rect')
+    // setting id for base tree node element
+    .attr('id', 'node')
+    // setting width for nodes
+    .attr('width', nodeWidth)
+    // setting height for nodes
+    .attr('height', nodeHeight)
+    // setting border radius
+    .attr('rx', '20')
+    .attr('ry', '20')
+    // setting stroke color
+    .style('stroke', 'black')
+    // setting stroke width
+    .style('stroke-width', 2)
+
+
+  // applying tree structure to hierarchy
+  d3.tree()
+    // setting nodes size
+    .nodeSize([
+      nodeHeight + nodeSeparation,
+      nodeWidth + nodeSeparation
+    ])(nodes)
+
+
+  // applying links to the plot
+  // creating empty selection of path elements
+  const links = g.selectAll("path")
+    // applying links data from hierarchy
+    .data(nodes.links())
+    // getting all new data connections
+    .enter()
+    // appending real path elements 
+    .append("path")
+    // setting path d param by using links generator
+    .attr('d', d => lnkMkr(d))
+    // setting stroke color
+    .style('stroke', '#a9a9b3' )
+    // setting fill property 
+    // (none value because it should be just line)
+    .style('fill', 'none')
+
+  // applying nodes elements
+  // creating empty selection
+  g.selectAll("g")
+    // applying nodes data from hierarchy
+    // .descendants returns entire hierarchy
+    .data( nodes.descendants() )
+    // getting all new data connections
+    .enter()
+    // calling node element component function
+    .call(renderItem)
+
+  // component function for tree nodes
+  function renderItem(selection) {
+    // creating wrapper for node elements
+    const g = selection.append('g')
+      // setting text positioning style
+      .style('text-anchor', 'middle')
+      // setting mouseenter callback
+      .on('mouseenter', function(e, d) {
+        // getting path from root node to current node
+        // current node is which has mouseover
+        const path = nodes.path(d)
+
+        // changing color for links (path elements)
+        // which connects nodes in path
+        links
+          // setting stroke style
+          .style(
+            'stroke',
+            // checking that both linked nodes are in selected path
+            d => path.includes(d.target) && path.includes(d.source) 
+              // setting specific color
+              ? '#D53E4F' : '#a9a9b3'
+          )
+
+        // changing fill color for nodes which in selected path
+        // selecting all rect in node elements in plot
+        // use elements should be selected because all nodes reusing rect
+        // element from defs
+        g.selectAll('use')
+          // applying fill style for every node element
+          .style(
+            'fill',
+            // checking is element included in selected path
+            // and selecting specific color
+            d => path.includes(d) ? '#D53E4F' : d.data.color
+          )
+          
+      })
+      // setting mouseleave event
+      // to reset all styles for links and nodes in plot
+      .on('mouseleave', function() {
+        g.selectAll('use')
+          .style('fill', d => d.data.color)
+        
+        links.style('stroke', '#a9a9b3')
+      })
+
+    // appending node element by using 
+    // base rect element from defs
+    // appending use element
+    g.append('use')
+      // setting href param on base element to reuse
+      .attr('href', '#node')
+      // setting x coord for rect element
+      // with some correction for better positioning
+      .attr('x', d => d.y - 60 )
+      // setting y coord for rect element with some correction
+      .attr('y', d => d.x - 25 )
+      // setting rect background color
+      .style('fill', d => d.data.color)
+      
+    // appending text element
+    g.append('text')
+      // coords are swapped for horizontal tree positioning
+      // setting x coord
+      .attr('x', d => d.y)
+      // setting y coord
+      .attr('y', d => d.x + 5)
+      // setting text
+      .text(d => d.data.name)
+  }
+  // zoom event callback
+  function onzoom({ transform }) {
+    console.log('asd')
+    // setting new transform coords for items container
+    // because this is an element which should be
+    // changed on zoom or scrolling
+    g.attr("transform", transform)
+  }
+}
+
+window.onload = makeTree
